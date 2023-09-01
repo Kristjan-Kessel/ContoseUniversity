eef79c2f81c07661a5d61d1f74a08c5607f447ef
opilane <opilane@HP-CZC321706N>
2023-09-01 11:36:42 +0300

    Add project files.

---
+++ ContoseUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContoseUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

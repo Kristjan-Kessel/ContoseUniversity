7549bfdc5ed7e9093772ff3db0159e0407aed088
Kristjan <79764375+Kristjan-Kessel@users.noreply.github.com>
2023-09-01 15:45:10 +0300

    Update startup

--- ContoseUniversity/Program.cs
+++ ContoseUniversity/Program.cs
@@ -1,33 +1,71 @@
 using ContoseUniversity.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDbContext<SchoolContext>(options => options
-.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    private static void Main(String[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        CreateDbIfNotExists(host);
+        
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddDbContext<SchoolContext>(options => options
+        .UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+
+    }
+
+    private static void CreateDbIfNotExists(IHost Host)
+    {
+        using (var scope = Host.Services.CreateScope())
+        {
+            var service = scope.ServiceProvider;
+            try
+            {
+                var context = service.GetRequiredService<SchoolContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = service.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "an error occured creating the database");
+                throw;
+            }
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder => {
+            webBuilder.UseStartup<Program>();
+        });
+    
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();

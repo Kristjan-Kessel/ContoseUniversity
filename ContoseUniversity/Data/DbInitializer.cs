6e6e769253ec3c78127f0c35a53c9b71c6df7351
Kristjan <79764375+Kristjan-Kessel@users.noreply.github.com>
2023-09-01 14:18:32 +0300

    add db intializer

---
+++ ContoseUniversity/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using ContoseUniversity.Models;
+
+namespace ContoseUniversity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return;
+            }
+
+            var students = new Student[]
+            {
+                new Student() {FirstMidName="Kaarel-Martin",LastName="Noole",EnrollmentDate=DateTime.Now},
+                new Student() {FirstMidName="Karl Umberto",LastName="Kats",EnrollmentDate=DateTime.Now},
+                new Student() {FirstMidName="Kristjan Georg",LastName="Kessel",EnrollmentDate=DateTime.Now},
+                new Student() {FirstMidName="Henri",LastName="Jervson",EnrollmentDate=DateTime.Now},
+                new Student() {FirstMidName="Kenneth-Marcus",LastName="Aljas",EnrollmentDate=DateTime.Now},
+            };
+
+            context.Students.AddRange(students);
+
+        }
+    }
+}

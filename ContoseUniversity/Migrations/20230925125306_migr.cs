f12cd6c8a802e4d93ab47478a91afed382429083
Opilane TTHK <opilane@tthk.ee>
2023-09-25 16:40:38 +0300

    Add department views

---
+++ ContoseUniversity/Migrations/20230925125306_migr.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContoseUniversity.Migrations
+{
+    public partial class migr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Department_DepartmentId",
+                table: "Course");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Department_DepartmentId",
+                table: "Course",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Department_DepartmentId",
+                table: "Course");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Course",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Department_DepartmentId",
+                table: "Course",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId");
+        }
+    }
+}
